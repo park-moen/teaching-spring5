b318345b5f09c1e21c456c7d7e57e1d90d10d0dc
park-moen <pmj6516@naver.com>
2023-09-07 17:39:59 +0900

    Initial commit

---
+++ src/main/java/com/teachingspring5/TeachingSpring5Application.java
@@ -0,0 +1,13 @@
+package com.teachingspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingSpring5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/teachingspring5/TeachingSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachingspring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingSpring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
